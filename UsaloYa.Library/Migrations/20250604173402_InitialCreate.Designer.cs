dc1c82567dc5716856051a97e54e600fe9af1b5c
JoseBg33 <jose.gbuenavista@gmail.com>
2025-06-04 11:36:33 -0600

    Actualizacion de migraciones

--- UsaloYa.Library/Migrations/20250604173402_InitialCreate.Designer.cs
+++ UsaloYa.Library/Migrations/20250604173402_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UsaloYa.Library.Models;
 
@@ -11,9 +12,11 @@ using UsaloYa.Library.Models;
 namespace UsaloYa.Library.Migrations
 {
     [DbContext(typeof(DBContext))]
-    partial class DBContextModelSnapshot : ModelSnapshot
+    [Migration("20250604173402_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
