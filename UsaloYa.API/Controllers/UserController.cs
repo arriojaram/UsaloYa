6761b26a81219399afd36eb5eb14fb95664bc64c
JoseBg33 <jose.gbuenavista@gmail.com>
2025-07-23 14:12:48 -0600

    SCRUM-13 Enviar correo de Bienvenida

--- UsaloYa.API/Controllers/UserController.cs
+++ UsaloYa.API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Template;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System.ComponentModel.DataAnnotations;
@@ -288,6 +289,7 @@ namespace UsaloYa.API.Controllers
         [AllowAnonymous]
         public async Task<IActionResult> RequestVerificationCodeEmail([FromHeader] string DeviceId, [FromBody] RequestVerificationCodeDto request)
         {
+            var templatePath = Path.Combine(_env.ContentRootPath, "Templates", "Notificacion.html");
             _logger.LogInformation("Recibido email: {Email}, code: {Code}, deviceId: {DeviceId}", request.Email, request.Code, DeviceId);
 
             try
@@ -295,6 +297,7 @@ namespace UsaloYa.API.Controllers
                 var (isValid, message, userId) = await _userService.RequestVerificationCodeEmail(request, DeviceId);
                 if (isValid == true)
                 {
+                    await _emailService.SendWelcomeEmail(request.Email, templatePath);
                     return Ok(new { isValid = isValid, userId = userId, message = message });
                 }
                 return BadRequest("No se logró verificar");
