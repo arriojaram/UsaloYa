224c14fe32312afa4e09c26961f4baf904835a32
Juan Manuel <jm_arrioja@hotmail.com>
2025-07-24 10:50:46 -0600

    Refactor code

--- DatabaseFiles/StoredProcedures/AddFolioSale.sql
+++ DatabaseFiles/StoredProcedures/AddFolioSale.sql
@@ -1,40 +1,26 @@
-SET ANSI_NULLS ON
-GO
 
-SET QUOTED_IDENTIFIER ON
-GO
-
-IF OBJECT_ID('[AddFolioSale]', 'P') IS NULL
+CREATE OR ALTER PROCEDURE [AddFolioSale]
+    @SaleId INT,
+    @CompanyId INT
+AS
 BEGIN
-    EXEC('
-    CREATE PROCEDURE [AddFolioSale]
-        @SaleId INT,
-        @CompanyId INT
-    AS
-    BEGIN
-        SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
-        DECLARE @NewFolio INT;
+    DECLARE @NewFolio INT;
 
-        SELECT @NewFolio = MAX(Folio)
-        FROM Sales
-        WHERE CompanyId = @CompanyId;
+    SELECT @NewFolio = MAX(Folio)
+    FROM Sales
+    WHERE CompanyId = @CompanyId;
 
-        IF @NewFolio IS NULL
-            SET @NewFolio = 1;
-        ELSE
-            SET @NewFolio = @NewFolio + 1;
+    IF @NewFolio IS NULL
+        SET @NewFolio = 1;
+    ELSE
+        SET @NewFolio = @NewFolio + 1;
 
-        UPDATE Sales
-        SET Folio = @NewFolio
-        WHERE SaleId = @SaleId;
+    UPDATE Sales
+    SET Folio = @NewFolio
+    WHERE SaleId = @SaleId;
 
-        SELECT @NewFolio AS NewFolio;
-    END
-    ')
-    PRINT 'Se creó el procedimiento de generación de folio';
-    RETURN;
+    SELECT @NewFolio AS NewFolio;
 END
 
-PRINT 'Ya existe el procedimiento de generación de folio';
-GO
